8d43cf706bf52440d9bcdffa3f07a1be30fb1bca
hiverkiya <hiverkiya@gmail.com>
2024-03-03 18:22:57 +0530

    Conditional return

--- pizza-menu/src/Pizza.js
+++ pizza-menu/src/Pizza.js
@@ -47,6 +47,7 @@ const pizzaData = [
 ];
 
 function Pizza(props) {
+  if (props.pizzaObject.soldOut) return null;
   return (
     <li className="pizza">
       <img src={props.pizzaObject.photoName} alt={props.pizzaObject.name} />
